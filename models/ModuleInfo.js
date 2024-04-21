80c5a7366d4eb79a0de73d9c068ba888b882e36b
789363 <40884505+789363@users.noreply.github.com>
2024-04-22 05:29:08 +0800

    Fix:ImportError

--- models/ModuleInfo.js
+++ models/ModuleInfo.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./index');  // 引入 Sequelize 实例
+const sequelize = require('./database');
 
 const ModuleInfo = sequelize.define('ModuleInfo', {
   module_id: {
