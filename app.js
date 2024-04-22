bf463adf5011010a1fa0a1184b9bfa8bbc84277d
789363 <40884505+789363@users.noreply.github.com>
2024-04-23 01:49:31 +0800

    Feat:RouterSet

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const { sequelize } = require('./models/database'); // 确保从正确的地方引入 sequelize 实例
+const moduleRoutes = require('./routes/moduleRoutes');
+const headerRoutes = require('./routes/headerRoutes');
+const machineRoutes = require('./routes/machineRoutes');  // 确保添加这些路由
+const lineRoutes = require('./routes/lineRoutes');
+const itemRoutes = require('./routes/itemRoutes');
+const resultRoutes = require('./routes/resultRoutes');
 
 const app = express();
+app.use(express.json());  // 用于解析 JSON 请求体
 
-app.use(express.json());
+// 使用路由
+app.use('/api', moduleRoutes);
+app.use('/api', headerRoutes);
+app.use('/api', machineRoutes);
+app.use('/api', lineRoutes);
+app.use('/api', itemRoutes);
+app.use('/api', resultRoutes);
 
-// 示例路由
-app.get('/', (req, res) => {
-  res.status(200).send('API is working');
-});
-
-// 监听端口
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  try {
-
-    console.log('Database connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
