e2c49bfb8ba17ddb29239657e05fb0960f6ab80f
789363 <40884505+789363@users.noreply.github.com>
2024-04-24 03:26:40 +0800

    Feat:Cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const moduleRoutes = require('./routes/moduleRoutes');
 const OpRoutes = require('./routes/OpRoutes');
+const cors = require('cors');
 const machineRoutes = require('./routes/machineRoutes');  // 确保添加这些路由
 const lineRoutes = require('./routes/lineRoutes');
 const itemRoutes = require('./routes/itemRoutes');
@@ -11,6 +12,10 @@ const swaggerFile = require('./swagger_output.json') // 剛剛輸出的 JSON
 
 
 const app = express();
+// 啟用 CORS
+app.use(cors({
+    origin: '*'  // 允許所有域的請求
+}));
 app.use(express.json());  // 用于解析 JSON 请求体
 
 // 使用路由
